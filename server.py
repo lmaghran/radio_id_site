9b3bc5df2944963c6bc188de52ae822151dd87cc
Lauren Maghran <maghranlauren@gmail.com>
2020-05-28 09:44:38 +0000

    imported year file

--- server.py
+++ server.py
@@ -8,13 +8,24 @@ app = Flask(__name__)
 app.secret_key= "ABC"
 
 
+# data.columns = ["a", "line_letter", "first_year", "etc.", "later_year", "d"]
+# data.drop("a", axis=1)
+# data.drop("etc.", axis=1)
+# data.drop("d", axis=1)
+# data.drop(data.index[0])
+
 @app.route("/", methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         whole_file= request.get_array(field_name='file')
         df = pd.DataFrame(whole_file[1:], columns=whole_file[0])
         df["Second_letter"]= df['Radio Serial Number'].astype(str).str[1]
-        print(df["Second_letter"])
+        data = pd.read_csv("year_from_site.txt", sep=" ", header=[0])
+        data.rename(columns={'Second':'Junk', 'is': 'Junk'}, inplace=True)
+
+        # df["Serial_year"]= year_data[df["Second_letter"].astype(str)]
+        # print(df["Serial_year"])
+        print(data.columns)
         return jsonify({"result":whole_file})
     return render_template("index.html")
 
